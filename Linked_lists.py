dd1677ea439684116dc3a52c704b667b9fcba44e
Osinachi Atama <osinachiatama@gmail.com>
2024-07-13 01:31:56 -0400

    added hello world and Linked list file containing append code

--- Linked_lists.py
+++ Linked_lists.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     company1.weight = 500
     company1.print_company()
 
-'''
+
 
 # ******************************************Definig  a class with parameters
 class Company: 
@@ -37,7 +37,7 @@ class Company:
 if __name__ == "__main__" : 
     company1 = Company("Turner", 100,500)
     company1.print_company()
-
+'''
 
 # ****************************************** Linked lists as dictionaries
 
@@ -67,3 +67,56 @@ if __name__ == "__main__" :
 #This will work for linked lists: 
 #print(my_linked_list.head.next.next.value)
 
+#**********************************************Linked lists 
+'''
+class Linkedlist : 
+    def __init__(self,value): #the word self in the function shows that this is a constructor and not a regular function
+        #creates new Node
+    def append(self, value): 
+        # create new Node 
+        #add Node to end
+    def prepend(self, value): 
+        # create new Node 
+        #add Node to the beginning
+     def insert(self,index, value): 
+        # create new Node 
+        # inset node at index
+'''
+class Node : 
+    def __init__(self,value) :
+        self.value = value 
+        self.next = None
+
+class LinkedList: 
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self. length = 1
+
+    #******************************Looking at all the values in a linked list 
+
+    def print_list(self): 
+        temp = self.head
+        while temp is not None: 
+            print(temp.value)
+            temp = temp.next
+
+        #****************************Appending an item to the end of a list
+
+    def append (self, value): 
+        new_node = Node(value)
+        if self.head is None: 
+            self.head = new_node 
+            self.tail = new_node 
+        else : 
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+
+my_linked_list = LinkedList(1)
+
+my_linked_list.append(2)
+
+my_linked_list.print_list()
